49ba48c824cfc12db2e24d18827590203640c757
Ismael Sosa Dominguez <ismael.sosa@toolfactory.net>
2021-06-15 18:30:05 +0200

    Remove and Sorts Usings

---
+++ CheapAwesome/ServiceCollectionExtensions/HttpClientServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using CheapAwesome.Infrastructure.BargainCouplesClient;
+using CheapAwesome.Utils.Refit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Refit;
+using System;
+
+namespace CheapAwesome.ServiceCollectionExtensions
+{
+    public static class HttpClientServiceCollectionExtension
+    {
+        public static IServiceCollection AddHttpClients(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<RefitClientFactory>();
+
+            services.AddHttpClient(nameof(IBargainCouplesClient), c =>
+            {
+                c.BaseAddress = new Uri("https://webbedsdevtest.azurewebsites.net/");
+            }).AddTypedClient(c => RestService.For<IBargainCouplesClient>(c));
+
+            return services;
+        }
+    }
+}
